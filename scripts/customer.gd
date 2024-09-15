5abae248aacb21cb77223179f73a5f1685ce4be6
SeyiTsa <seyilawal59@gmail.com>
2024-09-14 21:44:38 -0500

    test

--- scripts/customer.gd
+++ scripts/customer.gd
@@ -3,13 +3,15 @@ class_name Customer
 
 @onready var interact_area: Area2D = $"Interact Area"
 @onready var nav : NavigationAgent2D = $NavigationAgent2D
-@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+@onready var sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 var customer_interactions : Array = ["Follow", "Seat", "Order", "Serve"]
 var SPEED : int = 150
 var should_navigate : bool = false
 var seat : Chair
 var customer : Customer
+var is_sitting : bool
 
 func _ready() -> void:
 	customer = self
@@ -17,7 +19,7 @@ func _ready() -> void:
 	interact_area.area_exited.connect(on_area_exited)
 	interaction_array.append_array(customer_interactions)
 	current_interaction = interaction_array[0]
-	nav.target_desired_distance = 100
+	nav.target_desired_distance = 60
 	
 
 func _physics_process(delta: float) -> void:
@@ -30,6 +32,12 @@ func _physics_process(delta: float) -> void:
 				sprite_2d.flip_h = true
 			elif velocity.x > 0:
 				sprite_2d.flip_h = false
+			else:
+				if player:
+					if player.global_position.x > global_position.x:
+						sprite_2d.flip_h = false
+					elif player.global_position.x < global_position.x:
+						sprite_2d.flip_h = true
 				
 			if player:
 				nav.target_position = player.global_position
@@ -45,7 +53,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		if !should_navigate:
 			can_be_selected = true
-			
+	
 	if is_selected() and Input.is_action_just_pressed("ui_accept"):
 		match current_interaction:
 			"Follow":
@@ -75,22 +83,39 @@ func _physics_process(delta: float) -> void:
 				InteractionManager.customer_currently_following = false
 func navigate():
 	if should_navigate:
-		sprite_2d.play("Walk")
 		var dir = to_local(nav.get_next_path_position()).normalized()
+		update_animation_parameters(dir)
 		if !nav.is_navigation_finished():
 			velocity = dir * SPEED
 			move_and_slide()
 		else:
-			sprite_2d.stop()
 			velocity = Vector2.ZERO
 			match current_interaction:
 				"Seat":
 					z_index = 0
 					if !get_parent() is Chair:
 						reparent(seat)
-					sprite_2d.play("Sit")
 					global_position = seat.customer_marker.global_position
+					is_sitting = true
+					update_animation_parameters(Vector2.ZERO)
 					if seat.scale.x == -1:
 						sprite_2d.flip_h = true
 					else:
 						sprite_2d.flip_h = false
+	else:
+		var dir = to_local(nav.get_next_path_position()).normalized()
+		update_animation_parameters(dir)
+func update_animation_parameters(anim_dir : Vector2):
+	var direction = round(anim_dir)
+	if is_sitting == false:
+		animation_tree.set("parameters/conditions/idle", velocity == Vector2.ZERO)
+	else:
+		animation_tree.set("parameters/conditions/idle", false)
+	animation_tree.set("parameters/conditions/is_moving", velocity != Vector2.ZERO)
+	animation_tree.set("parameters/conditions/is_sitting", is_sitting)
+	animation_tree["parameters/Idle/blend_position"] = direction
+	animation_tree["parameters/Walk/blend_position"] = direction
+	animation_tree["parameters/Sit/blend_position"] = direction
+
+
+	
