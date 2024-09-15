6cde0357c6b519ddfcf5045060a71cbc3d557188
SeyiTsa <seyilawal59@gmail.com>
2024-09-14 21:43:31 -0500

    Added animations for the customers
    I configured the animations for the customer and and the player.

--- scripts/notepad.gd
+++ scripts/notepad.gd
@@ -3,6 +3,9 @@ extends PanelContainer
 var state : String
 @onready var anim: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	anim.play("RESET")
+
 func _on_mouse_entered() -> void:
 	if state != "Up":
 		anim.play("pop up")
